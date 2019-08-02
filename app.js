79c08f5e64468edb15de0feb96890aa392f693ac
Gabriela Barcena <g.barcena.v@gmail.com>
2019-08-01 20:41:14 -0500

    mcjdn

--- app.js
+++ app.js
@@ -1,37 +1,39 @@
-const express = require('express');
-const multer = require('multer');
-const ejs = require('ejs');
-const path = require('path');
+const express = require("express");
+const multer = require("multer");
+const ejs = require("ejs");
+const path = require("path");
 
-
-var VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
-var fs = require('fs');
+var VisualRecognitionV3 = require("watson-developer-cloud/visual-recognition/v3");
+var fs = require("fs");
 
 //set IBM cloud credentials in this section
 var visualRecognition = new VisualRecognitionV3({
-  version: '2019-05-29',
-  iam_apikey: 'Remplace aqui'
+  version: "2019-05-29",
+  iam_apikey: "IWexYzqCM2X5RCjO5chEFR-u3B5sYP9svoGaSeGzkXD5"
 });
 
 // Set The Storage Engine
 const storage = multer.diskStorage({
-  destination: './public/uploads/',
-  filename: function(req, file, cb){
-    cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  destination: "./public/uploads/",
+  filename: function(req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
   }
 });
 
 // Init Upload
 const upload = multer({
   storage: storage,
-  limits:{fileSize: 1000000},
-  fileFilter: function(req, file, cb){
+  limits: { fileSize: 1000000 },
+  fileFilter: function(req, file, cb) {
     checkFileType(file, cb);
   }
-}).single('myImage');
+}).single("myImage");
 
 // Check File Type
-function checkFileType(file, cb){
+function checkFileType(file, cb) {
   // Allowed ext
   const filetypes = /jpeg|jpg|png|gif/;
   // Check ext
@@ -39,10 +41,10 @@ function checkFileType(file, cb){
   // Check mime
   const mimetype = filetypes.test(file.mimetype);
 
-  if(mimetype && extname){
-    return cb(null,true);
+  if (mimetype && extname) {
+    return cb(null, true);
   } else {
-    cb('Error: Solo Imagenes!');
+    cb("Error: Solo Imagenes!");
   }
 }
 
@@ -50,124 +52,133 @@ function checkFileType(file, cb){
 const app = express();
 
 // EJS
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // Public Folder
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 
-app.get('/', (req, res) => res.render('index'));
+app.get("/", (req, res) => res.render("index"));
 var x;
-app.post('/upload', (req, res) => {
-
-
-  upload(req, res, (err) => {
-    if(err){
-      res.render('index', {
+app.post("/upload", (req, res) => {
+  upload(req, res, err => {
+    if (err) {
+      res.render("index", {
         msg: err
       });
     } else {
-      if(req.file == undefined){
-        res.render('index', {
-          msg: 'Error: Ningun Archivo Seleccionado!'
+      if (req.file == undefined) {
+        res.render("index", {
+          msg: "Error: Ningun Archivo Seleccionado!"
         });
       } else {
-        var images_file = fs.createReadStream(`./public/uploads/${req.file.filename}`);
-        var classifier_ids = ["Tiposdetaco_2107577272"];
+        var images_file = fs.createReadStream(
+          `./public/uploads/${req.file.filename}`
+        );
+        var classifier_ids = ["Tacos_1101416385"];
         console.log("datos de la imagen -- " + images_file);
         //
-          var params = {
+        var params = {
           images_file: images_file,
           classifier_ids: classifier_ids,
-          owners: 'IBM,me'
+          owners: "IBM,me"
         };
         //aqui tiene que guardar la respuesta de json del clasificador en algnu laod
         //hasata ahorita fucinoa
         visualRecognition.classify(params, function(err, response) {
-          if (err)
-            console.log(err);
-          else{
-
-
-          //x = JSON.parse(response);
-          x= response;
-          var whatClass = JSON.stringify(x.images[0].classifiers[0].classes[0].class, null, 2);
-          var score = JSON.stringify(x.images[0].classifiers[0].classes[0].score, null, 2);
-          var type_hierarchy = JSON.stringify(x.images[0].classifiers[0].classes[0].type_hierarchy, null, 2);
-          //xy = JSON.stringify(x.images[0].classifiers[0].classes[0].class, null, 2);
-      }
-
-
+          if (err) console.log(err);
+          else {
+            //x = JSON.parse(response);
+            x = response;
+            var whatClass = JSON.stringify(
+              x.images[0].classifiers[0].classes[0].class,
+              null,
+              2
+            );
+            var score = JSON.stringify(
+              x.images[0].classifiers[0].classes[0].score,
+              null,
+              2
+            );
+            var type_hierarchy = JSON.stringify(
+              x.images[0].classifiers[0].classes[0].type_hierarchy,
+              null,
+              2
+            );
+            //xy = JSON.stringify(x.images[0].classifiers[0].classes[0].class, null, 2);
+          }
 
           //console.log(JSON.stringify(response, null, 2))
-          res.render('index', {
-            msg: 'Archivo subido Correctamente!',
+          res.render("index", {
+            msg: "Archivo subido Correctamente!",
             file: `uploads/${req.file.filename}`,
             clas: whatClass,
             sc: score,
             th: type_hierarchy
-
           });
         });
         //
-
       }
     }
   });
-
 });
 
-app.post('/upload2', (req, res) => {
-
-
-  upload(req, res, (err) => {
-    if(err){
-      res.render('index', {
+app.post("/upload2", (req, res) => {
+  upload(req, res, err => {
+    if (err) {
+      res.render("index", {
         msg: err
       });
     } else {
-      if(req.file == undefined){
-        res.render('index', {
-          msg: 'Error: Ningun archivo seleccionado!'
+      if (req.file == undefined) {
+        res.render("index", {
+          msg: "Error: Ningun archivo seleccionado!"
         });
       } else {
-        var images_file = fs.createReadStream(`./public/uploads/${req.file.filename}`);
+        var images_file = fs.createReadStream(
+          `./public/uploads/${req.file.filename}`
+        );
         var classifier_ids = ["default"];
         console.log("datos de la imagen -- " + images_file);
         //
-          var params = {
+        var params = {
           images_file: images_file,
-          classifier_ids: classifier_ids,
+          classifier_ids: classifier_ids
         };
         //aqui tiene que guardar la respuesta de json del clasificador en algnu laod
         //hasata ahorita fucinoa
         visualRecognition.classify(params, function(err, response) {
-          if (err)
-            console.log(err);
-          else{
-
-
-          //x = JSON.parse(response);
-          x= response;
-          var whatClass = JSON.stringify(x.images[0].classifiers[0].classes[0].class, null, 2);
-          var score = JSON.stringify(x.images[0].classifiers[0].classes[0].score, null, 2);
-          var type_hierarchy = JSON.stringify(x.images[0].classifiers[0].classes[0].type_hierarchy, null, 2);
-          //xy = JSON.stringify(x.images[0].classifiers[0].classes[0].class, null, 2);
-      }
-
-
+          if (err) console.log(err);
+          else {
+            //x = JSON.parse(response);
+            x = response;
+            var whatClass = JSON.stringify(
+              x.images[0].classifiers[0].classes[0].class,
+              null,
+              2
+            );
+            var score = JSON.stringify(
+              x.images[0].classifiers[0].classes[0].score,
+              null,
+              2
+            );
+            var type_hierarchy = JSON.stringify(
+              x.images[0].classifiers[0].classes[0].type_hierarchy,
+              null,
+              2
+            );
+            //xy = JSON.stringify(x.images[0].classifiers[0].classes[0].class, null, 2);
+          }
 
           //console.log(JSON.stringify(response, null, 2))
-          res.render('index', {
-            msg: 'Archivo subido correctamente!',
+          res.render("index", {
+            msg: "Archivo subido correctamente!",
             file: `uploads/${req.file.filename}`,
             clas: whatClass,
             sc: score,
             th: type_hierarchy
-
           });
         });
         //
-
       }
     }
   });
